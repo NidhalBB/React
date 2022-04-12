7410cd581f15cb7727ad8211202478b24145afcc
NidhalBB <benbrahim_nidhal@outlook.com>
2022-04-12 15:09:32 +0100

    message

--- portfolio/src/Component/Navbar.js
+++ portfolio/src/Component/Navbar.js
@@ -9,11 +9,11 @@ import {
   FaLinkedinIn,
 } from 'react-icons/fa';
 import { HiOutlineMail } from 'react-icons/hi';
-import { BsFillPersonLinesFill } from 'react-icons/bs';
+import { Link } from 'react-scroll';
 
 export default function Navbar() {
   const [nav , setNave] = useState(false);
-  const handlClick = () =>setNave(!nav);
+  const handleClick = () =>setNave(!nav);
   return (
     <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
       <div>
@@ -21,30 +21,76 @@ export default function Navbar() {
       </div>
       {/*menu*/}
       <ul className='hidden md:flex'>
-        <li> Home </li>
-        <li> About </li>
-        <li> Skilles </li>
-        <li> Work </li>
-        <li> Contact </li>
+        <li>
+          <Link to='home' smooth={true} duration={500}>
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link to='about' smooth={true} duration={500}>
+            About
+          </Link>
+        </li>
+        <li>
+          <Link to='skills' smooth={true} duration={500}>
+            Skills
+          </Link>
+        </li>
+        <li>
+          <Link to='work' smooth={true} duration={500}>
+            Work
+          </Link>
+        </li>
+        <li>
+          <Link to='contact' smooth={true} duration={500}>
+            Contact
+          </Link>
+        </li>
       </ul>
 
-      <div onClick ={handlClick} className ='md:hidden z-10'>
-        {!nav ? <FaBars/> : <FaTimes/>}
+      {/* Hamburger */}
+      <div onClick={handleClick} className='md:hidden z-10'>
+        {!nav ? <FaBars /> : <FaTimes />}
       </div>
 
-
-
-      <ul  className={!nav ? 'hidden'
+      {/* Mobile menu */}
+      <ul
+        className={
+          !nav
+            ? 'hidden'
             : 'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'
         }
       >
-        <li> Home </li>
-        <li> About </li>
-        <li> Skilles </li>
-        <li> Work </li>
-        <li> Contact </li>
+        <li className='py-6 text-4xl'>
+          <Link onClick={handleClick} to='home' smooth={true} duration={500}>
+            Home
+          </Link>
+        </li>
+        <li className='py-6 text-4xl'>
+          {' '}
+          <Link onClick={handleClick} to='about' smooth={true} duration={500}>
+            About
+          </Link>
+        </li>
+        <li className='py-6 text-4xl'>
+          {' '}
+          <Link onClick={handleClick} to='skills' smooth={true} duration={500}>
+            Skills
+          </Link>
+        </li>
+        <li className='py-6 text-4xl'>
+          {' '}
+          <Link onClick={handleClick} to='work' smooth={true} duration={500}>
+            Work
+          </Link>
+        </li>
+        <li className='py-6 text-4xl'>
+          {' '}
+          <Link onClick={handleClick} to='contact' smooth={true} duration={500}>
+            Contact
+          </Link>
+        </li>
       </ul>
-
       <div className='hidden lg:flex fixed flex-col top-[35%] left-0'>
       <ul>
           <li className='w-[160px] h-[40px] flex justify-between items-center ml-[-110px] hover:ml-[-10px] duration-300 bg-blue-600'>
